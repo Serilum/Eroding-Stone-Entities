24012ef572266a29b9b750c334fcd6ac5cc056a2
Rick South <rick@natamus.com>
2023-12-12 10:21:05 +0100

    Eroding Stone Entities source code for 1.20.2.

---
+++ Common/src/main/java/com/natamus/erodingstoneentities/util/Util.java
@@ -0,0 +1,100 @@
+package com.natamus.erodingstoneentities.util;
+
+import com.natamus.erodingstoneentities.config.ConfigHandler;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity.RemovalReason;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Util {
+	private static final List<Block> iceblocks = new ArrayList<Block>(Arrays.asList(Blocks.ICE, Blocks.BLUE_ICE, Blocks.FROSTED_ICE, Blocks.PACKED_ICE));
+	
+	public static HashMap<Item, Item> erodeinto = new HashMap<Item, Item>();
+	
+	public static boolean populateArrays() {
+		String tosandstring = ConfigHandler.itemsWhichErodeIntoSand;
+		String toredsandstring = ConfigHandler.itemsWhichErodeIntoRedSand;
+		String toclaystring = ConfigHandler.itemsWhichErodeIntoClay;
+		
+		for (String itemstring : tosandstring.split(",")) {
+			ResourceLocation rl = new ResourceLocation(itemstring.trim().replaceAll("[^a-z0-9_.-:]", ""));
+			if (!BuiltInRegistries.ITEM.keySet().contains(rl)) {
+				System.out.println("[Eroding Stone Entities] Unable to find item for input '" + itemstring.trim() + "' to erode into sand in the Forge item registry. Ignoring it.");
+				continue;
+			}
+			
+			Item item = BuiltInRegistries.ITEM.get(rl);
+			erodeinto.put(item, Items.SAND);
+		}
+		
+		for (String itemstring : toredsandstring.split(",")) {
+			ResourceLocation rl = new ResourceLocation(itemstring.trim().replaceAll("[^a-z0-9_.-:]", ""));
+			if (!BuiltInRegistries.ITEM.keySet().contains(rl)) {
+				System.out.println("[Eroding Stone Entities] Unable to find item for input '" + itemstring.trim() + "' to erode into red sand in the Forge item registry. Ignoring it.");
+				continue;
+			}
+			
+			Item item = BuiltInRegistries.ITEM.get(rl);
+			erodeinto.put(item, Items.RED_SAND);
+		}
+		
+		for (String itemstring : toclaystring.split(",")) {
+			ResourceLocation rl = new ResourceLocation(itemstring.trim().replaceAll("[^a-z0-9_.-:]", ""));
+			if (!BuiltInRegistries.ITEM.keySet().contains(rl)) {
+				System.out.println("[Eroding Stone Entities] Unable to find item for input '" + itemstring.trim() + "' to erode into clay in the Forge item registry. Ignoring it.");
+				continue;
+			}
+			
+			Item item = BuiltInRegistries.ITEM.get(rl);
+			if (ConfigHandler.erodeIntoClayBlockInsteadOfClayBall) {
+				erodeinto.put(item, Items.CLAY);
+			}
+			else {
+				erodeinto.put(item, Items.CLAY_BALL);
+			}
+		}
+		
+		if (erodeinto.size() == 0) {
+			System.out.println("[Eroding Stone Entities] The erode into hashmap is empty. Something went wrong while parsing your config file. Mod disabled.");
+			return false;
+		}
+		return true;
+	}
+	
+	public static void transformItemEntity(Level world, ItemEntity ie) {
+		ItemStack stack = ie.getItem();
+		if (stack == null) {
+			return;
+		}
+		
+		Item item = stack.getItem();
+		if (item == null) {
+			return;
+		}
+		
+		if (!erodeinto.containsKey(item)) {
+			return;
+		}
+		
+		Item toitem = erodeinto.get(item);
+		
+		ItemEntity newie = new ItemEntity(world, ie.getX(), ie.getY(), ie.getZ(), new ItemStack(toitem, stack.getCount()));
+		ie.remove(RemovalReason.DISCARDED);
+		world.addFreshEntity(newie);
+	}
+	
+	public static boolean isIceBlock(Block block) {
+		return iceblocks.contains(block);
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/erodingstoneentities/neoforge/events/NeoForgeErodingEvent.java
@@ -0,0 +1,28 @@
+package com.natamus.erodingstoneentities.neoforge.events;
+
+import com.natamus.erodingstoneentities.events.ErodingEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent.LevelTickEvent;
+import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeErodingEvent {
+	@SubscribeEvent
+	public static void onWorldTick(LevelTickEvent e) {
+		Level level = e.level;
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		ErodingEvent.onWorldTick((ServerLevel)level);
+	}
+	
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		ErodingEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+}
