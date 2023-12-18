951f455c187330c4768205ae5c2e6228acd04faa
Rick South <rick@natamus.com>
2023-12-18 10:30:39 +0100

    (v4.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/erodingstoneentities/util/Reference.java
+++ Common/src/main/java/com/natamus/erodingstoneentities/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.erodingstoneentities.util;
 public class Reference {
 	public static final String MOD_ID = "erodingstoneentities";
 	public static final String NAME = "Eroding Stone Entities";
-	public static final String VERSION = "4.3";
+	public static final String VERSION = "4.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
