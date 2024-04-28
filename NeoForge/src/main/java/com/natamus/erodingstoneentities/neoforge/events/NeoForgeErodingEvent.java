4ec26916a5ee0ce72d81a32f6582681680f4ca8d
Rick South <rick@natamus.com>
2024-04-28 19:02:24 +0200

    (v4.5) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/erodingstoneentities/neoforge/events/NeoForgeErodingEvent.java
+++ NeoForge/src/main/java/com/natamus/erodingstoneentities/neoforge/events/NeoForgeErodingEvent.java
@@ -3,8 +3,7 @@ package com.natamus.erodingstoneentities.neoforge.events;
 import com.natamus.erodingstoneentities.events.ErodingEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.LevelTickEvent;
-import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -12,9 +11,9 @@ import net.neoforged.fml.common.EventBusSubscriber;
 @EventBusSubscriber
 public class NeoForgeErodingEvent {
 	@SubscribeEvent
-	public static void onWorldTick(LevelTickEvent e) {
-		Level level = e.level;
-		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+	public static void onWorldTick(LevelTickEvent.Pre e) {
+		Level level = e.getLevel();
+		if (level.isClientSide) {
 			return;
 		}
 
