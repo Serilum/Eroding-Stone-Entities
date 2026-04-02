69bcc1180b8ca33a14d5b11724bb7a248a3e2cdf
Rick South <rick@natamus.com>
2026-04-02 16:13:22 +0200

    Eroding Stone Entities source code for 26.1.1.

---
+++ Common/src/main/java/com/natamus/erodingstoneentities/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.erodingstoneentities;
+
+import com.natamus.erodingstoneentities.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
---
+++ Common/src/main/java/com/natamus/erodingstoneentities/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.erodingstoneentities.util;
+
+public class Reference {
+	public static final String MOD_ID = "erodingstoneentities";
+	public static final String NAME = "Eroding Stone Entities";
+	public static final String VERSION = "4.7";
+	public static final String ACCEPTED_VERSIONS = "[26.1.1]";
+}
